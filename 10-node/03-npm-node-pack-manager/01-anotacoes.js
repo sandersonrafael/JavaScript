480321df72d5334ecae4ec48fd81c34c0406590e
sandersonrafael <sandersonrafael-35@hotmail.com>
2023-03-12 21:31:24 -0300

    10.03.01

---
+++ 10-node/03-npm-node-pack-manager/01-anotacoes.js
@@ -0,0 +1,4 @@
+// inicia o projeto com npm init -y para gerar o json
+// agora para instalar o express: npm install express
+// se desejar fixar a versão, usa-se npm install express -E
+// npm update -> atualiza todos os pacotes do package.json para a versão mais atual
