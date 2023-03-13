e5ab357610cd849573dae94779e232ad63ea6058
sandersonrafael <sandersonrafael-35@hotmail.com>
2023-03-12 21:32:31 -0300

    Update 01-anotacoes.js

--- 10-node/03-npm-node-pack-manager/01-anotacoes.js
+++ 10-node/03-npm-node-pack-manager/01-anotacoes.js
@@ -2,3 +2,4 @@
 // agora para instalar o express: npm install express
 // se desejar fixar a versão, usa-se npm install express -E
 // npm update -> atualiza todos os pacotes do package.json para a versão mais atual
+ 
