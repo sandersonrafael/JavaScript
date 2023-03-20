2872b78df80b3153af3336af9c1f4f14a28ab4be
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-03-20 11:58:41 -0300

    A 151

--- 11-projeto-agenda/routes.js
+++ 11-projeto-agenda/routes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const route = express.Router()
-
 const homeController = require('./src/controllers/homeController')
-route.get('/', homeController.paginaInicial)
-route.post('/', homeController.trataPost)
+const loginController = require('./src/controllers/loginController')
 
-const contatoController = require('./src/controllers/contatoController')
-route.get('/contato', contatoController.getContato)
+
+// rotas home
+route.get('/', homeController.index)
+
+// rotas login
+route.get('/login/index', loginController.index)
 
 module.exports = route
