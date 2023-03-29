51659b33f61a59aedcbf65897e5c599d80997407
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-03-28 22:56:59 -0300

    Aluno Routes

--- 13-API_REST-js-node-e-SQL/01-API_REST/src/controllers/HomeController.js
+++ 13-API_REST-js-node-e-SQL/01-API_REST/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
     async index(req, res) {
-        const novoAluno = await Aluno.create({
-            nome: 'André',
-            sobrenome: 'Felipe',
-            email: 'dede@gmail.com',
-            idade: 30,
-            peso: 81,
-            altura: 1.78,
-        });
-        res.json(novoAluno);
+        res.json('Index');
     }
 }
 
