50bfdddfb5b2e5b5a8e873ec5ab124c9254d82b4
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-04-01 23:15:02 -0300

    Hello world!

---
+++ 14-react/notes/01-react.js
@@ -0,0 +1,15 @@
+/* create-react-app
+
+para iniciar um projeto com react, da maneira antiga (defasada), utilizar código:
+npx create-react-app nomeDoProjeto
+npx create-react-app . -> cria na própria pasta
+
+apagar todas os arquivos do src, menos os arquivos:
+App.css
+App.js -> aqui que será gerada a aplicação. limpar o arquivo e importar React from 'react'
+index.js
+
+para gerar uma função jsx (componentes html no js), usar funcion App () { sintaxe html }
+apagar o conteúdo do arquivo App.css
+
+*/
