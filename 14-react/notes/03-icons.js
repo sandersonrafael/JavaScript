a33ac44970db909cd98a8398a3128833dcd1812a
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-04-03 11:38:16 -0300

    14.1.3

---
+++ 14-react/notes/03-icons.js
@@ -0,0 +1,13 @@
+/* react-icons
+
+é possível utilizar uma biblioteca react para ter acesso a ícones. Basta instalar:
+
+npm install react-icons
+
+no documento, utilizar:
+import { nome do ícone } from 'react-icons/fa' (font awesome)
+
+para ter acesso aos nomes com ctrl + espaço, fazer:
+import {  } from 'react-icons/fa' -> ao apertar ctrl + espaço dentro das chaves, o vscode
+sugere os nomes
+*/
