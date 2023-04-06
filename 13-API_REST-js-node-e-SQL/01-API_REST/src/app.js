d58331ce6e16a4583dfc0669e5e4bd93fa5910b3
sandersonrafael <sandersonrafael-35@hotmail.com>
2023-04-06 17:00:48 -0300

    13.1.1

--- 13-API_REST-js-node-e-SQL/01-API_REST/src/app.js
+++ 13-API_REST-js-node-e-SQL/01-API_REST/src/app.js
@@ -4,11 +4,29 @@ import { resolve } from 'path';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
+
+const whiteList = [
+    'https://escola-react.sandersonrafae.xyz',
+    'http://localhost:3000',
+];
+
+const corsOptions = {
+    origin(origin, callback) {
+        if (whiteList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
 
 import './database';
 
@@ -20,6 +38,8 @@ class App {
     }
 
     middlewares() {
+        this.app.use(cors(corsOptions));
+        this.app.use(helmet());
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.json());
         this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
