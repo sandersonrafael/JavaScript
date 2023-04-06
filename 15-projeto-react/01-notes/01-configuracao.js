8491bca5054a8e0383b92db98f0ceab15ba1af2d
sandersonrafael <sandersonrafael-35@hotmail.com>
2023-04-06 20:33:17 -0300

    15.1.1

--- 15-projeto-react/01-notes/01-configuracao.js
+++ 15-projeto-react/01-notes/01-configuracao.js
@@ -1,3 +1,42 @@
+/* eslint, prettier e .editorconfig
+
+instalar todos através do npm num dev dependence:
+npm i @babel/eslint-plugin prettier eslint-config-prettier eslint-plugin-prettier -D
+npm install --save-dev @babel/preset-react
+*/
+
+/* config .editorconfig
+configurar o arquivo .editor config com:
+
+root = true
+
+[*]
+indent_style = space
+indent_size = 4
+end_of_line = lf
+charset = utf-8
+trim_trailing_whitespace = true
+insert_final_newline = true
+
+*/
+
+/* criar e configurar arquivo .prettierrc:
+
+{
+    "singleQuote": true,
+    "trailingComma": "es5"
+}
+
+*/
+
+/* editar o json do próprio VSCode para autocorreção. adicionar:
+
+"editor.codeActionsOnSave": {"source.fixAll.eslint": true}
+
+*/
+
+/* configurar o .eslint.js com:
+
 module.exports = {
     env: {
         browser: true,
@@ -29,3 +68,5 @@ module.exports = {
         'react-hooks/exhaustive-deps': 'warn',
     },
 };
+
+*/
