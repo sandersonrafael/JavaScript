508f1b6bacefa0e6ee34859e7cf9718480f6f7e7
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-04-07 15:54:15 -0300

    15.1.2

---
+++ 15-projeto-react/base-react/src/pages/Page404/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+
+export default function Page404() {
+    return (
+        <Container>
+            <h1>A página que você procura não existe.</h1>
+        </Container>
+    );
+}
