bd05415c9dd278706aed2e70ac9601b35c82e304
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-06-08 23:56:03 -0300

    api consuption started

---
+++ 15-projeto-react/consumindo-api/src/pages/Page404/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import history from '../../services/history';
+import { Container } from '../../styles/GlobalStyles';
+
+export default function Page404() {
+  history.push('/');
+
+  return (
+    <Container>
+      <h1>Página não encontrada</h1>
+    </Container>
+  );
+}
