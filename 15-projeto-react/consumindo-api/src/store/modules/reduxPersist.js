61182101807bd5c196d114dc8ece62950a6bcfa1
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-06-09 16:58:19 -0300

    Login #1

--- 15-projeto-react/consumindo-api/src/store/modules/reduxPersist.js
+++ 15-projeto-react/consumindo-api/src/store/modules/reduxPersist.js
@@ -4,9 +4,9 @@ import { persistReducer } from 'redux-persist';
 export default (reducers) => {
   const persistedReducers = persistReducer(
     {
-      key: 'BASE-REACT',
+      key: 'CONSUMINDO-API',
       storage,
-      whitelist: ['example'],
+      whitelist: ['auth'],
     },
     reducers
   );
