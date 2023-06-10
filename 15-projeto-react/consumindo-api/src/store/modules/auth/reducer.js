f087d25048eed2a4c0d346a97c0f0b7e76757a62
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-06-09 21:25:48 -0300

    Login #2

--- 15-projeto-react/consumindo-api/src/store/modules/auth/reducer.js
+++ 15-projeto-react/consumindo-api/src/store/modules/auth/reducer.js
@@ -2,7 +2,7 @@ import * as types from '../types';
 
 const initialState = {
   isLoggedIn: false,
-  token: false,
+  token: '',
   user: {},
   isLoading: false,
 };
@@ -10,8 +10,16 @@ const initialState = {
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case types.LOGIN_REQUEST: {
-      console.log('REDUCER', action.payload);
-      return state;
+      const newState = { ...state };
+      newState.isLoggedIn = true;
+      newState.token = action.payload.token;
+      newState.user = action.payload.user;
+      return newState;
+    }
+
+    case types.LOGIN_FAILURE: {
+      const newState = { ...initialState };
+      return newState;
     }
 
     default: {
